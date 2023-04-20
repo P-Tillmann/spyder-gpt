c2ee4c01e595f796dbac40a6719c9cbdb0b5d7c4
Tillmann <ojo@basisit.de>
2023-04-20 11:10:13 +0200

    added widgets for output, input and send button

--- spyder_gpt/spyder/widgets.py
+++ spyder_gpt/spyder/widgets.py
@@ -23,6 +23,18 @@ from spyder.api.widgets.main_widget import PluginMainWidget
 # Localization
 _ = get_translation("spyder_gpt.spyder")
 
+from qtpy.QtWidgets import QTextBrowser, QWidget, QVBoxLayout, QSizePolicy, QSpacerItem, QPushButton
+from spyder.api.widgets.mixins import SpyderWidgetMixin
+
+from spyder.api.widgets.status import BaseTimerStatus
+from spyder.utils.icon_manager import ima
+
+# Third party imports
+import qtawesome as qta
+
+
+from qtpy.QtWidgets import QPlainTextEdit, QWidget, QVBoxLayout, QTextEdit
+from spyder.api.widgets.mixins import SpyderWidgetMixin
 
 class GPTinSpyderActions:
     ExampleAction = "example_action"
@@ -45,12 +57,44 @@ class GPTinSpyderWidget(PluginMainWidget):
     def __init__(self, name=None, plugin=None, parent=None):
         super().__init__(name, plugin, parent)
 
-        # Create an example label
-        self._example_label = QLabel("Example Label")
+       
 
-        # Add example label to layout
-        layout = QHBoxLayout()
-        layout.addWidget(self._example_label)
+        self.browser = QTextBrowser()
+        self.browser.setPlaceholderText("Test Text Browser")
+
+        # Create a vertical layout and add the output widget to it
+        
+        self.editor = QPlainTextEdit()
+
+        # set the size policy for the editor widget
+        editor_size_policy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        editor_size_policy.setVerticalStretch(0)
+        editor_size_policy.setVerticalPolicy(QSizePolicy.Fixed)
+        editor_size_policy.setHorizontalStretch(1)
+        self.editor.setSizePolicy(editor_size_policy)
+
+        # set the height of the editor widget to 30% of the height of the plugin widget
+        editor_height = int(self.height() * 0.3)
+        print(f"Editor height: {editor_height}")
+        
+        self.editor.setFixedHeight(300)
+        
+        #self.editor.set
+        
+        spacer = QSpacerItem(0, 0, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.button = QPushButton("Sent")
+        self.button.clicked.connect(self.display_text)
+
+        layout = QVBoxLayout()
+        
+        layout.addWidget(self.browser)
+        #layout.addItem(spacer)
+        layout.addWidget(self.editor)
+        layout.addWidget(self.button)
+
+        # Create a vertical layout and add the input widget to it
+        
         self.setLayout(layout)
 
     # --- PluginMainWidget API
@@ -60,6 +104,13 @@ class GPTinSpyderWidget(PluginMainWidget):
 
     def get_focus_widget(self):
         pass
+    
+    def display_text(self):
+       # get the text from the editor widget
+       text = self.editor.toPlainText()
+
+       # set the HTML content of the browser widget to the text
+       self.browser.setHtml(text)
 
     def setup(self):
         # Create an example action
